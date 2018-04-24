ed1ec6b436c0b5f3405953d871ab232dafad61a3
zhoudiqiu <zhoudiqiu@wustl.edu>
2018-04-24 16:29:37 -0500

    add flat script

--- flat_directory.sh
+++ flat_directory.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 START_TIME=$(date +%s%N)
+start=1
+
+mkdir {1..2000}
+./monitor_flat_directory_structure {1..2000}
 for num in {1..2000}
 do
-touch ./test1/testfile
-rm ./test1/testfile
-touch ./test2/testfile
-rm ./test2/testfile
+touch ./$num/testfile
+rm ./$num/testfile
+
 done
 ELAPSED_TIME=$((($(date +%s%N) - $START_TIME)/1000000))
+rm -rf {1..2000}
 echo "Time taken: $ELAPSED_TIME ms"
 exit 0
