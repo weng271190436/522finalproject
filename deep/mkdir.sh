ebb58a7a6b7781c30533e9c0b9094647524307cd
weng271190436 <wengweiwei2006@gmail.com>
2018-04-24 23:12:01 -0500

    recursively make directories

---
+++ deep/mkdir.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+make_dir()
+{
+	if [[ $1 -gt 0 ]]
+	then
+		mkdir 1
+		cd 1
+		make_dir $[$1 - 1]
+		cd ..
+		mkdir 2
+		cd 2
+		make_dir $[$1 - 1]
+		cd ..
+	fi
+}
+
+mkdir deep
+cd deep
+make_dir 3
+
+exit 0
